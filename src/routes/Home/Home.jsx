5e98571df1ebc8cce3105b46f9da375bde59de50
Rolando Medina <rolando.medina.romo@gmail.com>
2022-06-16 23:40:21 -0500

    Changed font size in the main tag of Layout component
    Now the style of the default text is set inline in the Home component. So it's easier to delete this style and continue working without the default style affecting all the project.
    Before the font size was defined in Layout.scss main { font-size: 5rem } so every font was huge.

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -3,7 +3,27 @@ import React from "react";
 import "./Home.scss";
 
 function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+        fontSize: "5rem",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "5rem",
+          fontWeight: "normal",
+        }}
+      >
+        Hello World!
+      </h1>
+    </div>
+  );
 }
 
 export { Home };
